efd1d5e4f29464c72b1be26a53ab9ac76ac01181
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2026-07-06 16:50:47 +0530

    Update main.py

--- practice_jwt/main.py
+++ practice_jwt/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/")
-def home():
-  return {"message":"hello world"}
-
 @app.get("/health")
 def health():
   return {"message":"server running"}
+
+@app.post("/login")
+def login(data:login):
+  return {
+    "username":data.username,
+    "password":data.password
+  }
 
 
