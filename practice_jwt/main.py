cb989b5a86e989b87e97e251a20c21f0db56a6f6
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2026-07-07 18:54:34 +0530

    Update main.py

--- practice_jwt/main.py
+++ practice_jwt/main.py
@@ -1,22 +1,61 @@
-from fastapi import FastAPI
-from schemas import Login
+from fastapi import FastAPI,HTTPException,Depends,Header
+from jose import JWTError,jwt
+from datetime import datetime,timedelta,timezone
+
 app=FastAPI()
+
+SECRET_KEY="mysecret"
+
+ALGORITHM="HS256"
+
 
 @app.get("/health")
 def health():
   return {"message":"server running"}
 
-@app.post("/login")
-def login(data:Login):
-  return {
-    "username":data.username,
-    "password":data.password
-  }
 
-@app.post("/register")
-def register(data.Register):
-  return {
-    "name":data.name,
-    "email":data.email,
-    "password":data.password
-  }
+#create token
+def create_token(data:dict):
+    to_encode=data.copy()
+    expire =datetime.now(timezone.utc)+timedelta(minutes=30)
+    to_encode.update({
+        "exp":expire
+    })
+    token=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return token
+
+
+#Login API (Token Generate)
+@app.post("/login")
+def login(username:str,password:str):
+    # print(username,password)
+    if username!="admin" or password!="1234":
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username and password"
+        )
+    token=create_token({
+        "sub":username
+    })
+    return {
+        "access_token":token
+    }
+
+#Token verify
+def verify_token(token:str=Header(None)):
+    try:
+        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        return payload
+    except:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+
+@app.get("/secure")
+def secure_data(user=Depends(verify_token)):
+    return {
+        "message":"secure data accessed",
+        "user":user
+    }
+
